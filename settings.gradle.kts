70eed3f1761cfad94fcf4e7ec92eaade90897d61
BaxtersLab <165230507+BaxtersLab@users.noreply.github.com>
2026-02-18 15:31:40 -0800

    Refactor settings.gradle.kts to simplify plugin management
    Removed centralized plugin management for Kotlin versions.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,16 +6,8 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
-}
-
-// Centralized plugin version pins per Master Constitution
-pluginManagement {
     plugins {
         id("org.jetbrains.kotlin.jvm") version "1.9.10"
         id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
-    }
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
     }
 }
